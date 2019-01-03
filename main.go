836db83fc22bae4385fdf8065608d0a466d34ecc
jaketaylor <jake@privategrity.com>
2019-01-03 18:15:38 -0500

    Added main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package main
+
+// import "gitlab.com/elixxir/registration/cmd"
+
+func main() {
+	// cmd.Execute()
+}
