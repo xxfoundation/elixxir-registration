21aeac54d290bf09621d75a39c289d57196e4e1c
josh <josh@elixxir.io>
2019-07-16 13:41:31 -0700

    work more on tests, add an already signed cert (see ~/provisioning/test.sh)

--- cert_authority/ca_test.go
+++ cert_authority/ca_test.go
@@ -10,7 +10,18 @@ func TestSign_VerifySignature(t *testing.T) {
 
 }
 
-//Test all the file opening things?
+//Test all the file opening things? Almost certainly a waste of time *shrugs*??
+func Test_LoadCert(t *testing.T)  {
+
+}
+
+func TestLoadCSR(t *testing.T)  {
+
+}
+
+func TestLoadPrivKey(t *testing.T)  {
+
+}
 
 /*
 func TestSign_FileIsValidCert(t *testing.T) {
