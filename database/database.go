bd4e44894b1e17a310bf45ef84984cffbb700336
josh <josh@elixxir.io>
2019-11-27 14:44:24 -0800

    Remove permissioningDb from implementation Use map backend for NewDatabase only

--- database/database.go
+++ database/database.go
@@ -10,7 +10,6 @@ package database
 
 import (
 	"github.com/go-pg/pg"
-	"github.com/go-pg/pg/orm"
 	jww "github.com/spf13/jwalterweatherman"
 	"sync"
 )
@@ -79,65 +78,11 @@ type NodeInformation struct {
 
 // Initialize the Database interface with database backend
 func NewDatabase(username, password, database, address string) Database {
-	// Create the database connection
-	db := pg.Connect(&pg.Options{
-		User:         username,
-		Password:     password,
-		Database:     database,
-		Addr:         address,
-		MaxRetries:   10,
-		MinIdleConns: 1,
+	jww.INFO.Println("Using map backend for UserRegistry!")
+	return Database(&MapImpl{
+		client: make(map[string]*RegistrationCode),
+		node:   make(map[string]*NodeInformation),
 	})
-
-	// Ensure an empty NodeInformation table
-	err := db.DropTable(&NodeInformation{},
-		&orm.DropTableOptions{IfExists: true})
-	if err != nil {
-		// If an error is thrown with the database, run with a map backend
-		jww.ERROR.Printf("Unable to initalize database backend: %+v", err)
-		jww.INFO.Println("Using map backend for UserRegistry!")
-		return Database(&MapImpl{
-			client: make(map[string]*RegistrationCode),
-			node:   make(map[string]*NodeInformation),
-		})
-	}
-
-	// Initialize the schema
-	jww.INFO.Println("Using database backend for Permissioning!")
-	err = createSchema(db)
-	if err != nil {
-		jww.FATAL.Panicf("Unable to initialize database backend: %+v", err)
-	}
-
-	// Return the database-backed Database interface
-	jww.INFO.Println("Database backend initialized successfully!")
-	return Database(&DatabaseImpl{
-		db: db,
-	})
-}
-
-// Create the database schema
-func createSchema(db *pg.DB) error {
-	for _, model := range []interface{}{&RegistrationCode{}, &NodeInformation{}} {
-		err := db.CreateTable(model, &orm.CreateTableOptions{
-			// Ignore create table if already exists?
-			IfNotExists: true,
-			// Create temporary table?
-			Temp: false,
-			// FKConstraints causes CreateTable to create foreign key constraints
-			// for has one relations. ON DELETE hook can be added using tag
-			// `sql:"on_delete:RESTRICT"` on foreign key field.
-			FKConstraints: true,
-			// Replaces PostgreSQL data type `text` with `varchar(n)`
-			// Varchar: 255
-		})
-		if err != nil {
-			// Return error if one comes up
-			return err
-		}
-	}
-	// No error, return nil
-	return nil
 }
 
 // Adds Client registration codes to the database
