6cdbf6d4a58600795fbef8f62d86fa0130a94e26
josh <josh@elixxir.io>
2019-12-02 15:57:04 -0800

    Reconfigure database architecture
    Node registration no longer needs to persist, so a map backend suffices. Database was reconfigured for this while still supporting client registration being persistant through a database

--- database/database.go
+++ database/database.go
@@ -10,6 +10,7 @@ package database
 
 import (
 	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
 	jww "github.com/spf13/jwalterweatherman"
 	"sync"
 )
@@ -19,6 +20,8 @@ type DatabaseImpl struct {
 	db *pg.DB // Stored database connection
 }
 
+//Fixme: This the real issue, need to remove this??
+
 // Struct implementing the Database Interface with an underlying Map
 type MapImpl struct {
 	client map[string]*RegistrationCode
@@ -26,15 +29,15 @@ type MapImpl struct {
 	mut    sync.Mutex
 }
 
-// Global variable for database interaction
-var PermissioningDb Database
+type Permissioning struct {
+	nodeRegistration
+	clientRegistration
+}
 
-// Interface database storage operations
-type Database interface {
-	// Inserts Client registration code with given number of uses
-	InsertClientRegCode(code string, uses int) error
-	// If Client registration code is valid, decrements remaining uses
-	UseCode(code string) error
+// Global variable for database interaction
+var PermissioningDb Storage
+
+type nodeRegistration interface {
 	// If Node registration code is valid, add Node information
 	InsertNode(id []byte, code, serverCert,
 		gatewayAddress, gatewayCert string) error
@@ -44,6 +47,19 @@ type Database interface {
 	CountRegisteredNodes() (int, error)
 	// Get Node information for the given Node registration code
 	GetNode(code string) (*NodeInformation, error)
+}
+
+type clientRegistration interface {
+	// Inserts Client registration code with given number of uses
+	InsertClientRegCode(code string, uses int) error
+	// If Client registration code is valid, decrements remaining uses
+	UseCode(code string) error
+}
+
+// Interface database storage operations
+type Storage struct {
+	clientRegistration
+	nodeRegistration
 }
 
 // Struct representing a RegistrationCode table in the database
@@ -75,12 +91,80 @@ type NodeInformation struct {
 }
 
 // Initialize the Database interface with database backend
-func NewDatabase(username, password, database, address string) Database {
-	jww.INFO.Println("Using map backend for UserRegistry!")
-	return Database(&MapImpl{
-		client: make(map[string]*RegistrationCode),
-		node:   make(map[string]*NodeInformation),
+func NewDatabase(username, password, database, address string) Storage {
+	//TODO: Start up regCode database and make a map backend
+
+	// Create the database connection
+	db := pg.Connect(&pg.Options{
+		User:         username,
+		Password:     password,
+		Database:     database,
+		Addr:         address,
+		MaxRetries:   10,
+		MinIdleConns: 1,
 	})
+
+	_ = Storage{
+		clientRegistration: clientRegistration(&MapImpl{
+			client: make(map[string]*RegistrationCode),
+		}),
+		nodeRegistration: nodeRegistration(&MapImpl{
+			node: make(map[string]*NodeInformation),
+		})}
+	// Initialize the schema
+	jww.INFO.Println("Using database backend for Permissioning!")
+	err := createSchema(db)
+	if err != nil {
+		// Return the map-backend interface
+		// in the event there is a database error
+		jww.ERROR.Printf("Unable to initialize database backend: %+v", err)
+		jww.INFO.Println("Using map backend for UserRegistry!")
+		return Storage{
+			clientRegistration: clientRegistration(&MapImpl{
+				client: make(map[string]*RegistrationCode),
+			}),
+			nodeRegistration: nodeRegistration(&MapImpl{
+				node: make(map[string]*NodeInformation),
+			})}
+	}
+
+	regCodeDb := &DatabaseImpl{
+		db: db,
+	}
+	nodeMap := &MapImpl{
+		client: make(map[string]*RegistrationCode),
+	}
+
+	jww.INFO.Println("Database/map backend initialized successfully!")
+	return Storage{
+		regCodeDb,
+		nodeMap,
+	}
+
+}
+
+// Create the database schema
+func createSchema(db *pg.DB) error {
+	for _, model := range []interface{}{&RegistrationCode{}} {
+		err := db.CreateTable(model, &orm.CreateTableOptions{
+			// Ignore create table if already exists?
+			IfNotExists: true,
+			// Create temporary table?
+			Temp: false,
+			// FKConstraints causes CreateTable to create foreign key constraints
+			// for has one relations. ON DELETE hook can be added using tag
+			// `sql:"on_delete:RESTRICT"` on foreign key field.
+			FKConstraints: true,
+			// Replaces PostgreSQL data type `text` with `varchar(n)`
+			// Varchar: 255
+		})
+		if err != nil {
+			// Return error if one comes up
+			return err
+		}
+	}
+	// No error, return nil
+	return nil
 }
 
 // Adds Client registration codes to the database
