f2cf6d6bdb28ff19973382af74686d196b93a230
jaketaylor <jake@privategrity.com>
2020-05-06 13:42:29 -0700

    fix test

--- cmd/registration_test.go
+++ cmd/registration_test.go
@@ -95,8 +95,6 @@ func TestEmptyDataBase(t *testing.T) {
 			"Expected %s, Recieved: %+v", expectedErr, err)
 		return
 	}
-	impl.Comms.Shutdown()
-
 }
 
 // Happy path: looking for a code that is in the database
