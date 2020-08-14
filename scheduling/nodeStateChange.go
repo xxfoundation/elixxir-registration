4b479d46e7b617cabc3e3ad56f85bd6b336f4098
Niamh Nikali <niamh@elixxir.io>
2020-08-14 09:36:50 -0700

    Move stuck fix to new file

--- scheduling/nodeStateChange.go
+++ scheduling/nodeStateChange.go
@@ -278,33 +278,3 @@ func killRound(state *storage.NetworkState, r *round.State,
 
 	return err
 }
-
-// Detect nodes that are stuck on waiting with no current round running
-// Then, add them back to the active pool so they can run rounds again
-func unstickNodes(state *storage.NetworkState, pool *waitingPool, roundTimeout time.Duration) {
-	// Check states of all nodes
-	nodeStates := state.GetNodeMap().GetNodeStates()
-	for i := range nodeStates {
-		thisNode := nodeStates[i]
-		_, thisRound := nodeStates[i].GetCurrentRound()
-		// add the node back to the waiting pool if certain conditions are met
-		if thisRound != nil && time.Since(thisRound.GetLastUpdate()) > 2*roundTimeout && thisNode.GetStatus() == node.Active && thisNode.GetActivity() == current.WAITING {
-			thisNode.ClearRound()
-			pool.Add(thisNode)
-		}
-	}
-}
-
-// Runner that unsticks nodes periodically
-// Exported methods should have only exported types for params! Right?
-func UnstickNodes(state *storage.NetworkState, pool *waitingPool, roundTimeout time.Duration, quitChan chan struct{}) {
-	unstickNodeTicker := time.NewTicker(2 * roundTimeout)
-	for cont := true; cont; {
-		select {
-		case <-unstickNodeTicker.C:
-			unstickNodes(state, pool, roundTimeout)
-		case <-quitChan:
-			cont = false
-		}
-	}
-}
