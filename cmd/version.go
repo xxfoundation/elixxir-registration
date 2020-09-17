98ff39c44aad92b345d6d483037d7ca5de9c5429
josh <josh@elixxir.io>
2020-09-17 15:10:24 -0700

    Update minor version number

--- cmd/version.go
+++ cmd/version.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "1.4.1"
+const currentVersion = "1.5.1"
 
 func printVersion() {
 	fmt.Printf("xx network Permissioning Server v%s -- %s\n\n",
