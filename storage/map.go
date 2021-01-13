9eb35061844323d6324e537e21604e7039e8125e
Jake Taylor <jake@elixxir.io>
2021-01-13 12:26:34 -0600

    resolve build issues

--- storage/map.go
+++ storage/map.go
@@ -9,23 +9,9 @@
 
 package storage
 
-import (
-	jww "github.com/spf13/jwalterweatherman"
-)
-
 // Initialize the Database interface with Database backend
 // Returns a Storage interface, Close function, and error
 func NewDatabase(username, password, database, address,
 	port string) (Storage, func() error, error) {
-	defer jww.INFO.Println("Map backend initialized successfully!")
-	return Storage{
-		&MapImpl{
-			applications: make(map[uint64]*Application),
-			nodes:        make(map[string]*Node),
-			nodeMetrics:  make(map[uint64]*NodeMetric),
-			roundMetrics: make(map[uint64]*RoundMetric),
-			clients:      make(map[string]*RegistrationCode),
-			users:        make(map[string]bool),
-			states:       make(map[string]string),
-		}}, func() error { return nil }, nil
+	return NewMap(), func() error { return nil }, nil
 }
