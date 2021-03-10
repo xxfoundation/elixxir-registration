edec8afa45d278f4b85c9523887db72d0d50be6b
Benjamin Wenger <ben@elixxir.ioo>
2021-03-10 07:22:14 -0800

    fixed a bug with banned nodes

--- scheduling/schedule.go
+++ scheduling/schedule.go
@@ -15,6 +15,7 @@ import (
 	"gitlab.com/elixxir/primitives/states"
 	"gitlab.com/elixxir/registration/storage"
 	"gitlab.com/elixxir/registration/storage/node"
+	"gitlab.com/elixxir/registration/storage/round"
 	"gitlab.com/xx_network/comms/signature"
 	"gitlab.com/xx_network/primitives/id"
 	"sync/atomic"
