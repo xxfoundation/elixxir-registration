d342fe29be6abc4970409b9e375b31a7b7d11c1a
Sydney Anne Erickson <sydney@elixxir.io>
2021-08-02 10:15:59 -0700

    Add functions for looking up city and coords

--- cmd/geoBin.go
+++ cmd/geoBin.go
@@ -8,14 +8,15 @@
 package cmd
 
 import (
+	"strconv"
+	"sync/atomic"
+
 	"github.com/oschwald/geoip2-golang"
 	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/registration/storage"
 	"gitlab.com/elixxir/registration/storage/node"
 	"gitlab.com/xx_network/primitives/utils"
-	"strconv"
-	"sync/atomic"
 )
 
 // Error messages.
@@ -95,6 +96,92 @@ func lookupCountry(ipAddr string, geoIPDB *geoip2.Reader) (string, error) {
 
 	// Return the two letter alpha-2 country code
 	return country.Country.IsoCode, nil
+}
+
+// getAddressCity returns the city for an IP address. Panics if
+// randomGeoBinning is not set or a geoip2.Reader is not provided.
+func getAddressCity(ipAddr string, geoIPDB *geoip2.Reader, geoipStatus *geoipStatus) (string, error) {
+	if geoIPDB != nil {
+		// Return an error if the status is not set to running (meaning the
+		// reader has been closed)
+		if !geoipStatus.IsRunning() {
+			return "", errors.New(ipdbNotRunningErr)
+		}
+
+		// Get country code for the country of the node's IP address
+		city, err := lookupCity(ipAddr, geoIPDB)
+		if err != nil {
+			return "", errors.Errorf(countryLookupErr, err)
+		}
+		return city, nil
+	}
+
+	err := errors.New(invalidFlagsErr)
+	jww.FATAL.Panic("Cannot get node bins: " + err.Error())
+
+	return "", err
+}
+
+// lookupCity returns the city of where the address is
+// located as found in the GeoIP2 database.
+func lookupCity(ipAddr string, geoIPDB *geoip2.Reader) (string, error) {
+	// Parse the IP string into a net.IP object
+	ip := utils.ParseIP(ipAddr)
+	if ip == nil {
+		return "", errors.Errorf(parseIpErr, ipAddr)
+	}
+
+	// Get the node's country from its IP address via the GeoIP2 database
+	country, err := geoIPDB.City(ip)
+	if err != nil {
+		return "", errors.Errorf(ipdbErr, err)
+	}
+
+	// Return the city
+	return country.City.Names["en"], nil
+}
+
+// getAddressCoords returns the coords for an IP address. Panics if
+// randomGeoBinning is not set or a geoip2.Reader is not provided.
+func getAddressCoords(ipAddr string, geoIPDB *geoip2.Reader, geoipStatus *geoipStatus) (float64, float64, error) {
+	if geoIPDB != nil {
+		// Return an error if the status is not set to running (meaning the
+		// reader has been closed)
+		if !geoipStatus.IsRunning() {
+			return 0.0, 0.0, errors.New(ipdbNotRunningErr)
+		}
+
+		// Get country code for the country of the node's IP address
+		latitude, longitude, err := lookupCoords(ipAddr, geoIPDB)
+		if err != nil {
+			return 0.0, 0.0, errors.Errorf(countryLookupErr, err)
+		}
+		return latitude, longitude, nil
+	}
+
+	err := errors.New(invalidFlagsErr)
+	jww.FATAL.Panic("Cannot get node bins: " + err.Error())
+
+	return 0.0, 0.0, err
+}
+
+// lookupCoords returns the coords of where the address approx. is
+// located as found in the GeoIP2 database. Latitude, then Longitude
+func lookupCoords(ipAddr string, geoIPDB *geoip2.Reader) (float64, float64, error) {
+	// Parse the IP string into a net.IP object
+	ip := utils.ParseIP(ipAddr)
+	if ip == nil {
+		return 0.0, 0.0, errors.Errorf(parseIpErr, ipAddr)
+	}
+
+	// Get the node's country from its IP address via the GeoIP2 database
+	record, err := geoIPDB.City(ip)
+	if err != nil {
+		return 0.0, 0.0, errors.Errorf(ipdbErr, err)
+	}
+
+	// Return the city
+	return record.Location.Latitude, record.Location.Longitude, nil
 }
 
 // geoipStatus signals the status of the GeoIP2 database reader. It should be
