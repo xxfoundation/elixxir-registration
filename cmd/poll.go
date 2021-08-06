06d83cdddfab25077a7e8319523a851e136d4318
josh <josh@elixxir.io>
2021-08-06 11:26:24 -0700

    Fix auth mistake in poll

--- cmd/poll.go
+++ cmd/poll.go
@@ -38,6 +38,11 @@ func (m *RegistrationImpl) Poll(msg *pb.PermissioningPoll, auth *connect.Auth) (
 			"is nil, poll cannot be processed")
 	}
 
+	// Ensure poller is properly authenticated
+	if !auth.IsAuthenticated {
+		return response, connect.AuthError(auth.Sender.GetId())
+	}
+
 	// Check for correct version
 	err := checkVersion(m.params, msg)
 	if err != nil {
