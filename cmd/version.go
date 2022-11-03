477e34919fd349d45d52573afcd2c984f036cf26
joshemb <josh@elixxir.io>
2022-11-03 09:52:35 -0700

    Update minor version for go upgrade

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "3.4.1"
+const currentVersion = "3.5.0"
 
 func printVersion() {
 	fmt.Printf("xx network Permissioning Server v%s -- %s\n\n",
